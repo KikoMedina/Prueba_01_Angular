17becfeb110392d18edc77bb80d6afe66915f3fd
KikoMedina <89874363+KikoMedina@users.noreply.github.com>
2022-11-16 14:40:05 +0100

    1 file modified, 1 added

---
+++ src/app/pedals.ts
@@ -0,0 +1,14 @@
+import { Pedal } from './pedal';
+
+export const PEDALS: Pedal[] = [
+  { id: 2, name: 'RAT ProCo 2' },
+  { id: 3, name: 'Ibanez TS9' },
+  { id: 4, name: 'Fulltone Deja-Vibe' },
+  { id: 5, name: 'Fulltone Octafuzz OF2' },
+  { id: 6, name: 'Electro Harmonix Big Muff' },
+  { id: 7, name: 'MXR Phase 90' },
+  { id: 8, name: 'Boss DD-7' },
+  { id: 9, name: 'Crybaby Dunlop' },
+  { id: 10, name: 'Klon ktr Centaur overdrive' }
+
+];
