e91a935c4baab8728d05494870dbbcc95e899101
KikoMedina <89874363+KikoMedina@users.noreply.github.com>
2022-11-22 15:38:46 +0100

    2 files modified

--- src/app/pedals.ts
+++ src/app/pedals.ts
@@ -1,13 +1,13 @@
 import { Pedal } from './pedal';
 
 export const PEDALS: Pedal[] = [
-  { id: 2, name: 'RAT ProCo 2' },
-  { id: 3, name: 'Ibanez TS9' },
-  { id: 4, name: 'Fulltone Deja-Vibe' },
-  { id: 5, name: 'Fulltone Octafuzz OF2' },
-  { id: 6, name: 'Electro Harmonix Big Muff' },
-  { id: 7, name: 'MXR Phase 90' },
-  { id: 8, name: 'Boss DD-7' },
-  { id: 9, name: 'Crybaby Dunlop' },
-  { id: 10, name: 'Klon ktr Centaur overdrive' }
+  { id: 1, name: 'RAT ProCo 2' },
+  { id: 2, name: 'Ibanez TS9' },
+  { id: 3, name: 'Fulltone Deja-Vibe' },
+  { id: 4, name: 'Fulltone Octafuzz OF2' },
+  { id: 5, name: 'Electro Harmonix Big Muff' },
+  { id: 6, name: 'MXR Phase 90' },
+  { id: 7, name: 'Boss DD-7' },
+  { id: 8, name: 'Crybaby Dunlop' },
+  { id: 9, name: 'Klon ktr Centaur overdrive' }
 ];
