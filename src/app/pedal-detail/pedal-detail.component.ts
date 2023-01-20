d417a66bdb2955703fb7d13d0d7c0e40c8596bfe
KikoMedina <89874363+KikoMedina@users.noreply.github.com>
2023-01-20 16:45:27 +0100

    1 file modified

--- src/app/pedal-detail/pedal-detail.component.ts
+++ src/app/pedal-detail/pedal-detail.component.ts
@@ -1,17 +1,32 @@
 import { Pedal } from '../pedal';
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input } from '@angular/core';
 
-@Component ({
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { PedalService } from '../pedal.service';
+
+@Component({
   selector: 'app-pedal-detail',
   templateUrl: './pedal-detail.component.html',
-  styleUrls: ['./pedal-detail.component.css']
+  styleUrls: ['./pedal-detail.component.css'],
 })
 export class PedalDetailComponent implements OnInit {
   @Input() pedal?: Pedal;
 
-  constructor()  { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: PedalService,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void  { 
+  ngOnInit(): void {
+    this.getPedal();
   }
-
+  
+  getPedal(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pedalService.getPedal(id)
+      .subscribe(pedal => this.pedal = pedal);
+  }
 }
