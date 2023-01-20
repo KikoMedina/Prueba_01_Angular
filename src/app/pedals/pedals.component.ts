4e53147eab0fe7d9b7c5b54999ff340a3b0e5b88
KikoMedina <89874363+KikoMedina@users.noreply.github.com>
2023-01-20 16:26:13 +0100

    PedalsComponent pedal links

--- src/app/pedals/pedals.component.ts
+++ src/app/pedals/pedals.component.ts
@@ -9,24 +9,16 @@ import { MessageService } from '../message.service';
   styleUrls: ['./pedals.component.css'],
 })
 export class PedalsComponent implements OnInit {
-  selectedPedal?: Pedal;
   pedals: Pedal[] = [];
 
   constructor(
-    private pedalService: PedalService,
-    private messageService: MessageService
-  ) {}
+    private pedalService: PedalService) {}
 
   ngOnInit(): void {
     this.getPedals();
   }
 
-  onSelect(pedal: Pedal): void {
-    this.selectedPedal = pedal;
-    this.messageService.add(`PedalsComponent: Selected pedal id=${pedal.id}`);
-  }
-
   getPedals(): void {
-    this.pedalService.getPedals().subscribe((pedals) => (this.pedals = pedals));
+    this.pedalService.getPedals().subscribe(pedals => this.pedals = pedals);
   }
 }
