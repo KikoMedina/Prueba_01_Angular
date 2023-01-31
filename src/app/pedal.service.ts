07462bf7a475476be7a6960ff35614e5bb5d69b8
KikoMedina <89874363+KikoMedina@users.noreply.github.com>
2023-01-31 14:52:14 +0100

    Create catchError function

--- src/app/pedal.service.ts
+++ src/app/pedal.service.ts
@@ -9,6 +9,8 @@ import { MessageService } from './message.service';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -21,9 +23,9 @@ export class PedalService {
   ) {}
 
   getPedals(): Observable<Pedal[]> {
-    const pedals = of(PEDALS);
-    this.messageService.add('PedalService: fetched pedals');
-    return pedals;
+    return this.http
+      .get<Pedal[]>(this.pedalsUrl)
+      .pipe(catchError(this.handleError<Pedal[]>('getPedals', [])));
   }
   getPedal(id: number): Observable<Pedal> {
     const pedal = PEDALS.find((p) => p.id === id)!;
@@ -32,5 +34,17 @@ export class PedalService {
   }
   private log(message: string) {
     this.messageService.add(`PedalService: ${message}`);
+  }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
