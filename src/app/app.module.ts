9c13823bfd890f7ef9f7607f35ca46a9355e1d93
KikoMedina <89874363+KikoMedina@users.noreply.github.com>
2023-02-03 15:02:52 +0100

    Fix App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { PedalsComponent } from './pedals/pedals.component';
-import { PedalDetailComponent } from './pedal-detail/pedal-detail.component';
-import { PedalSearchComponent } from './pedal-search/pedal-search.component';
-import { MessagesComponent } from './messages/messages.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PedalDetailComponent } from './pedal-detail/pedal-detail.component';
+import { PedalsComponent } from './pedals/pedals.component';
+import { PedalSearchComponent } from './pedal-search/pedal-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
@@ -22,8 +37,12 @@ import { HttpClientModule } from '@angular/common/http';
     MessagesComponent,
     PedalSearchComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
