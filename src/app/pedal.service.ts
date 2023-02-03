721dc680e47b56b2536a33fb3d6f806a6b0ce7e8
KikoMedina <89874363+KikoMedina@users.noreply.github.com>
2023-02-03 12:56:46 +0100

    Refactor pedal.service.ts

--- src/app/pedal.service.ts
+++ src/app/pedal.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-
-import { Observable, of } from 'rxjs';
-
-import { Pedal } from './pedal';
-import { PEDALS } from './mock-pedals';
-
-import { MessageService } from './message.service';
-
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+
+import { Pedal } from './pedal';
+import { MessageService } from './message.service';
+
+
+
+
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +17,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class PedalService {
   private pedalsUrl = 'api/pedals';
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
   constructor(
     private http: HttpClient,
     private messageService: MessageService
@@ -27,6 +30,18 @@ export class PedalService {
       .get<Pedal[]>(this.pedalsUrl)
       .pipe(catchError(this.handleError<Pedal[]>('getPedals', [])));
   }
+  getPedalNo404<Data>(id: number): Observable<Pedal> {
+    const url = `${this.pedalsUrl}/?id=${id}`;
+    return this.http.get<Pedal[]>(url)
+      .pipe(
+        map(pedals => pedals[0]), // returns a {0|1} element array
+        tap(p => {
+          const outcome = p ? 'fetched' : 'did not find';
+          this.log(`${outcome} pedal id=${id}`);
+        }),
+        catchError(this.handleError<Pedal>(`getPedal id=${id}`))
+      );
+  }
   getPedal(id: number): Observable<Pedal> {
     const url = `${this.pedalsUrl}/${id}`;
     return this.http.get<Pedal>(url).pipe(
@@ -34,50 +49,9 @@ export class PedalService {
       catchError(this.handleError<Pedal>(`getPedal id=${id}`))
     );
   }
-  private log(message: string) {
-    this.messageService.add(`PedalService: ${message}`);
-  }
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
-  /** PUT: update the pedal on the server */
-  updatePedal(pedal: Pedal): Observable<any> {
-    return this.http.put(this.pedalsUrl, pedal, this.httpOptions).pipe(
-      tap((_) => this.log(`updated pedal id=${pedal.id}`)),
-      catchError(this.handleError<any>('updatePedal'))
-    );
-  }
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-  };
-  /** POST: add a new pedal to the server */
-  addPedal(pedal: Pedal): Observable<Pedal> {
-    return this.http.post<Pedal>(this.pedalsUrl, pedal, this.httpOptions).pipe(
-      tap((newPedal: Pedal) => this.log(`added pedal w/ id=${newPedal.id}`)),
-      catchError(this.handleError<Pedal>('addPedal'))
-    );
-  }
-
-  /** DELETE: delete the pedal from the server */
-  deletePedal(id: number): Observable<Pedal> {
-    const url = `${this.pedalsUrl}/${id}`;
-
-    return this.http.delete<Pedal>(url, this.httpOptions).pipe(
-      tap((_) => this.log(`deleted pedal id=${id}`)),
-      catchError(this.handleError<Pedal>('deletePedal'))
-    );
-  }
-  /* GET pedals whose name contains search term */
-  searchPedals(term: string): Observable<Pedal[]> {
+   /* GET pedals whose name contains search term */
+   searchPedals(term: string): Observable<Pedal[]> {
     if (!term.trim()) {
       // if not search term, return empty hero array.
       return of([]);
@@ -91,4 +65,49 @@ export class PedalService {
       catchError(this.handleError<Pedal[]>('searchHeroes', []))
     );
   }
+
+     //////// Save methods //////////
+
+   /** POST: add a new pedal to the server */
+   addPedal(pedal: Pedal): Observable<Pedal> {
+    return this.http.post<Pedal>(this.pedalsUrl, pedal, this.httpOptions).pipe(
+      tap((newPedal: Pedal) => this.log(`added pedal w/ id=${newPedal.id}`)),
+      catchError(this.handleError<Pedal>('addPedal'))
+    );
+  }
+
+   /** DELETE: delete the pedal from the server */
+   deletePedal(id: number): Observable<Pedal> {
+    const url = `${this.pedalsUrl}/${id}`;
+
+    return this.http.delete<Pedal>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted pedal id=${id}`)),
+      catchError(this.handleError<Pedal>('deletePedal'))
+    );
+  }
+
+    /** PUT: update the pedal on the server */
+    updatePedal(pedal: Pedal): Observable<any> {
+      return this.http.put(this.pedalsUrl, pedal, this.httpOptions).pipe(
+        tap((_) => this.log(`updated pedal id=${pedal.id}`)),
+        catchError(this.handleError<any>('updatePedal'))
+      );
+    }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  private log(message: string) {
+    this.messageService.add(`PedalService: ${message}`);
+  }
+
 }
